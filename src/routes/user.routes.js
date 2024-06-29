1a219e14ecdf16ed231894983d2b76cf7422562e
AshishRajput-A <ashishrajput982476@gmail.com>
2024-06-29 11:21:20 +0530

    adding subpipline and  creating router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import {
+  registerUser,
+  updateAccountDetails,
   loginUser,
   logoutuser,
   getdata,
   refreshAccessToken,
+  getWatchHistory,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
 } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { varifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -28,15 +35,38 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 // secures route
-
 router.route("/logout").post(varifyJWT, logoutuser);
 
 //refresh accesstoken
-
 router.route("/refreshtoken", refreshAccessToken);
 
 //get all data
-
 router.route("/getdata").get(getdata);
+
+//change password
+router.route("/change-password").post(varifyJWT, changeCurrentPassword);
+
+//get current user
+router.route("/currentuser").get(varifyJWT, getCurrentUser);
+
+//update account details
+router.route("/update-account").patch(varifyJWT, updateAccountDetails);
+
+//update avatar
+router
+  .route("/update-avatar")
+  .patch(varifyJWT, upload.single("avatar"), updateUserAvatar);
+
+//update coverImage
+router
+  .route("/update-coverImage")
+  .patch(varifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+//get user profile
+router.route("/c/:username").get(varifyJWT, getUserChannelProfile);
+
+//get watchhistory
+
+router.route("/hostory").get(varifyJWT, getWatchHistory);
 
 export default router;
